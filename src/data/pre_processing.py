42cd34d631520621fc593c935f241a414fc13025
mashwaniT <78741056+mashwaniT@users.noreply.github.com>
2023-04-02 02:23:29 -0400

    cpu too slow trying to train on gpu

--- src/data/pre_processing.py
+++ src/data/pre_processing.py
@@ -1,3 +1,4 @@
+
 import os
 import cv2
 import numpy as np
@@ -5,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MultiLabelBinarizer
 from keras.preprocessing.image import ImageDataGenerator
 import pandas as pd
+from iterstrat.ml_stratifiers import MultilabelStratifiedKFold
 
 
 def get_preprocessed_data():
@@ -30,8 +32,12 @@ def get_preprocessed_data():
 
     return np.array(images)
 
-   csv_file_path = "../data/raw/sample_labels.csv"
+   csv_file_path = os.path.join("..", "..", "data", "raw", "sample_labels.csv")
    df = pd.read_csv(csv_file_path)
+
+   class_counts = df['Finding Labels'].value_counts()
+   classes_to_keep = class_counts[class_counts >= 2].index
+   df = df[df['Finding Labels'].isin(classes_to_keep)]
 
    # creating train, validation and test sets
    train_df, test_df = train_test_split(
@@ -47,15 +53,17 @@ def get_preprocessed_data():
    stratify=train_df["Finding Labels"])
 
    # Load and preprocess images
-   image_dir = "../data/raw/images"
+   image_dir = os.path.join("..", "..", "data", "raw", "images")
    X_train = load_and_preprocess_images(train_df, image_dir)
+   X_train = np.expand_dims(X_train, axis=-1) # add a channel dim to the end of the array
    X_val = load_and_preprocess_images(val_df, image_dir)
+   X_val = np.expand_dims(X_val, axis=-1)
    X_test = load_and_preprocess_images(test_df, image_dir)
+   X_test = np.expand_dims(X_test, axis=-1)
 
-   X_train_resnet = load_and_preprocess_images_rgb(train_df, image_dir, image_size=(224, 224))
-   X_val_resnet = load_and_preprocess_images_rgb(val_df, image_dir, image_size=(224, 224))
-   X_test_resnet = load_and_preprocess_images_rgb(test_df, image_dir, image_size=(224, 224))
-
+   X_train_rgb = load_and_preprocess_images_rgb(train_df, image_dir, image_size=(224, 224))
+   X_val_rgb = load_and_preprocess_images_rgb(val_df, image_dir, image_size=(224, 224))
+   X_test_rgb = load_and_preprocess_images_rgb(test_df, image_dir, image_size=(224, 224))
 
    # encoding labels using one-hot encoding
    mlb = MultiLabelBinarizer()
@@ -75,7 +83,19 @@ def get_preprocessed_data():
 
    val_datagen = ImageDataGenerator()
 
-   return train_df, val_df, test_df, X_train, X_val, X_test, y_train, y_val, y_test, mlb, train_datagen, val_datagen
+   # multi-label stratified kfold
+   n_splits = 5
+   mskf = MultilabelStratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
+
+   # Find the first fold to use as the train and test sets
+   for train_index, test_index in mskf.split(df, mlb.transform(df["Finding Labels"])):
+      train_df = df.iloc[train_index]
+      test_df = df.iloc[test_index]
+      break
+
+
+   return train_df, val_df, test_df, X_train, X_val, X_test, y_train, y_val, y_test, mlb, train_datagen, val_datagen, X_train_rgb, X_val_rgb, X_test_rgb
+
 
 
 
